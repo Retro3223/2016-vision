e67bb2cdc5b7cd596482642f3d083ba8ab63295e
Ellery Newcomer <ellery-newcomer@utulsa.edu>
2016-02-29 02:12:12 +0000

    need to delay initializing structure sensor
    gremlins, probably

--- etc/visionstart.sh
+++ etc/visionstart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-sleep 10s
+# something about structure sensor doesn't work during bootup
+# maybe need delay?
+sleep 20s
 /usr/bin/python3 /opt/2016-vision/file_streamer.py 2>&1 > /var/log/vision.log 
